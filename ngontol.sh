d3a1a5145ed0bd6533b9310394cece78d2dee818
messiey <156035205+messiey@users.noreply.github.com>
2024-02-11 06:34:08 +0700

    Create ngontol.sh

---
+++ ngontol.sh
@@ -0,0 +1,14 @@
+    apt update -y && apt upgrade -y
+    apt install python3 python3-pip git speedtest-cli -y
+    pip3 install setuptools
+    pip3 install flask
+    pip install requests subprocess
+    sudo apt-get install -y p7zip-full
+    cd /usr/bin
+    clear
+    wget https://raw.githubusercontent.com/messiey/lele/master/ngontol.zip
+    unzip ngontol.zip
+    cd agin
+    pip3 install -r agin/requirements.txt
+  
+    
