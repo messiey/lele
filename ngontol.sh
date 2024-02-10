17bdd02f19fe16c3a4bae73521686a7c8e8d3f3d
messiey <156035205+messiey@users.noreply.github.com>
2024-02-11 06:47:27 +0700

    Update ngontol.sh

--- ngontol.sh
+++ ngontol.sh
@@ -8,4 +8,43 @@
     unzip lele/ngontol.zip
     pip3 install -r agin/requirements.txt
     pip3 install pillow
-    
+
+    #isi data
+echo "INSTALL BOT CREATE via TELEGRAM"
+read -e -p "[*] Input your Bot Token : " bottoken
+read -e -p "[*] Input Your Id Telegram :" admin
+read -e -p "[*] Input Your Domain :" domain
+echo -e BOT_TOKEN='"'$bottoken'"' >> /root/agin/var.txt
+echo -e ADMIN='"'$admin'"' >> /root/agin/var.txt
+echo -e DOMAIN='"'$domain'"' >> /root/agin/var.txt
+clear
+echo "Done"
+echo "Your Data Bot"
+echo -e "==============================="
+echo "Api Token     : $bottoken"
+echo "ID            : $admin"
+echo "DOMAIN        : $domain"
+echo -e "==============================="
+echo "Setting done"
+
+cat > /etc/systemd/system/agin.service << END
+[Unit]
+Description=Simple agin - @error404project
+After=network.target
+
+[Service]
+WorkingDirectory=/root
+ExecStart=/usr/bin/python3 -m agin
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl start agin
+systemctl enable agin
+
+clear
+
+echo " Installations complete, type /menu on your bot"
+
