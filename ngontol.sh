1a216fc6a0fa1a7cb76d82595b98f149cdac2271
messiey <156035205+messiey@users.noreply.github.com>
2024-02-11 21:04:00 +0700

    Update ngontol.sh

--- ngontol.sh
+++ ngontol.sh
@@ -2,7 +2,6 @@
 
 wget -q -O /usr/bin/add-ip "https://raw.githubusercontent.com/messiey/lele/master/add-ip.sh" && chmod +x /usr/bin/add-ip
 clear
-cd /usr/bin
 wget https://raw.githubusercontent.com/messiey/lele/master/jembot.sh && chmod +x jembot.sh && ./jembot.sh
 
 rm -f ngontol.sh
