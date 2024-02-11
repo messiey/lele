ce8b8cc32d777e24d145f520d33349a18f91447d
messiey <156035205+messiey@users.noreply.github.com>
2024-02-12 00:51:40 +0700

    Add files via upload

---
+++ error404project.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#install
+apt update && apt upgrade
+apt install python3 python3-pip git
+git clone https://github.com/Zeadxt/dbasu.git
+unzip lele/error404project.zip
+pip3 install -r error404project/requirements.txt
+pip3 install pillow
+
+#isi data
+echo ""
+read -e -p "[*] Input your Bot Token : " bottoken
+read -e -p "[*] Input Your Id Telegram :" admin
+read -e -p "[*] Input Your Domain :" domain
+echo -e BOT_TOKEN='"'$bottoken'"' >> /root/error404project/var.txt
+echo -e ADMIN='"'$admin'"' >> /root/error404project/var.txt
+echo -e DOMAIN='"'$domain'"' >> /root/error404project/var.txt
+clear
+echo "Done"
+echo "Your Data Bot"
+echo -e "==============================="
+echo "DOMAIN         : $bottoken"
+echo "Email          : $admin"
+echo "Api Key        : $domain"
+echo -e "==============================="
+echo "Setting done"
+
+cat > /etc/systemd/system/error404project.service << END
+[Unit]
+Description=Simple Error404Project - @Error404Project
+After=network.target
+
+[Service]
+WorkingDirectory=/root
+ExecStart=/usr/bin/python3 -m error404project
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl start error404project 
+systemctl enable error404project
+
+sleep 3
+
+clear
+
+echo " Installations complete, type /menu on your bot"
