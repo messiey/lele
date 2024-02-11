7cf8a32203fc83dfa4f30a6a9f6e40e13b37cdca
messiey <156035205+messiey@users.noreply.github.com>
2024-02-12 00:50:41 +0700

    Delete ngontol.sh

--- ngontol.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget -q -O /usr/bin/add-ip "https://raw.githubusercontent.com/messiey/lele/master/add-ip.sh" && chmod +x /usr/bin/add-ip
-clear
-wget https://raw.githubusercontent.com/messiey/lele/master/jembot.sh && chmod +x jembot.sh && ./jembot.sh
-
-rm -f ngontol.sh
