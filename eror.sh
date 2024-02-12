9a45a33ad66bfe43d3a90b1077a59acd2d4c984e
messiey <156035205+messiey@users.noreply.github.com>
2024-02-12 08:22:38 +0700

    Update eror.sh

--- eror.sh
+++ eror.sh
@@ -3,10 +3,10 @@
 #install
 
 apt update && apt upgrade
-apt install python3 python3-pip git
+sudo apt-get install python3 python3-pip -y
 cd /usr/bin
-git clone https://github.com/messiey/lele.git
-unzip lele/error.zip
+wget https://github.com/messiey/lele/master/eror.zip
+unzip error.zip
 pip3 install -r error/requirements.txt
 pip3 install python-telegram-bot==13.7
 pip3 install pillow
